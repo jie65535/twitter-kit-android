cc3f13e2cbe21892024c43eaf3aa0a3709fe730e
fanhonglin <fanhonglin@ifun.com>
2024-01-04 21:11:15 +0800

    android 29 works.

--- samples/twitterdemo/src/main/java/com/fanhl/twitterdemo/TwitterDemoActivity.java
+++ samples/twitterdemo/src/main/java/com/fanhl/twitterdemo/TwitterDemoActivity.java
@@ -2,11 +2,12 @@ package com.fanhl.twitterdemo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
